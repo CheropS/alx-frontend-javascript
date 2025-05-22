4c9da85669417fb7f43d80e6203f0f20619b70c5
CheropS <sh.cherop@gmail.com>
2025-05-22 16:09:07 +0300

    add director

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,28 @@
+// Creating instances of Teacher
+var teacher1 = {
+    firstName: "Alice",
+    lastName: "Johnson",
+    fullTimeEmployee: true,
+    yearsOfExperience: 10,
+    location: "New York",
+    contract: true // Additional custom attribute
+};
+var teacher2 = {
+    firstName: "Bob",
+    lastName: "Smith",
+    fullTimeEmployee: false,
+    location: "California",
+    contract: false // Additional custom attribute
+};
+// Create a Director object
+var director1 = {
+    firstName: "John",
+    lastName: "Doe",
+    location: "London",
+    fullTimeEmployee: true,
+    numberOfReports: 17
+};
+// Display Teacher objects
+console.log(teacher1);
+console.log(teacher2);
+console.log(director1);
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -25,7 +25,21 @@ const teacher2: Teacher = {
     location: "California",
     contract: false  // Additional custom attribute
 };
+// Extend Teacher to create Directors interface
+interface Directors extends Teacher {
+    numberOfReports: number;  // Mandatory attribute
+}
+
+// Create a Director object
+const director1: Directors = {
+    firstName: "John",
+    lastName: "Doe",
+    location: "London",
+    fullTimeEmployee: true,
+    numberOfReports: 17
+};
 
 // Display Teacher objects
 console.log(teacher1);
 console.log(teacher2);
+console.log(director1);
